624ac16ec1c7fb86123d210bb6306a29a49c762b
kmurthy432 <38903530+kmurthy432@users.noreply.github.com>
2018-05-02 16:18:36 +0530

    eaxmplae

---
+++ src/test/java/FinalProject/CompleteStuff/Example1.java
@@ -0,0 +1,26 @@
+public class Example1 {
+	
+	static WebDriver driver;
+	
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+
+		
+		System.setProperty("webdriver.chrome.driver", "Drivers\\chromedriver.exe");
+		
+		 driver= new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("https://login.cat.com/CwsLogin/cws/login.htm?appid=login");
+		driver.findElement(By.id("cwsUID")).sendKeys("ghumas");
+		driver.findElement(By.id("cwsPwd")).sendKeys("Sonali.deloitte@1");
+		driver.findElement(By.id("submitButton")).click();
+		
+		Thread.sleep(8500);
+		
+		System.out.println("current URL Is : " +driver.getCurrentUrl());
+		
+	
+		
+	}
